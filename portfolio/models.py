b2e1b23adca8769c0d45b810f436b470f1f57861
josealonzo2004 <josealonzohp@gmail.com>
2024-05-15 00:55:11 -0500

    SEGUNDO PROYECTO1

---
+++ portfolio/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+from django.db.models.fields import CharField, URLField
+from cloudinary.models import CloudinaryField
+
+# Create your models here.
+class Project(models.Model):
+    title = CharField(max_length=100)
+    description = CharField(max_length=250)
+    image = CloudinaryField('image')
+    url = URLField(blank=True)
+    
